b1966a9b1f76fee6a3caaa0621c6f9eaf0145a2e
AnanyaSharma29 <sharmananya02@gmail.com>
2024-12-27 11:53:25 +0530

    Revert "Add files via upload"
    This reverts commit 4848c8ea598868ac34813634ceb4acf60802f771.

--- src/main/java/com/EmergencyAndMentalWellBeing/Backend/Repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EmergencyAndMentalWellBeing.Backend.Repository;
-
-import com.EmergencyAndMentalWellBeing.Backend.Model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
--- src/main/java/com/EmergencyAndMentalWellBeing/Backend/Repository/SosAlertService.java
+++
@@ -1,8 +0,0 @@
-package com.EmergencyAndMentalWellBeing.Backend.Repository;
-
-import com.EmergencyAndMentalWellBeing.Backend.Model.SosAlert;
-
-public interface SosAlertService {
-    boolean sendSosNotification(SosAlert sosAlert);
-}
-
